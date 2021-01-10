9239d5e398db72603f09076e410e98081fa71c23
vgfang <vincent_fang@sfu.ca>
2021-01-09 21:40:54 -0800

    added mulitple image upload for mobile, img count command

--- bot.py
+++ bot.py
@@ -29,42 +29,60 @@ async def on_ready():
 			f'{guild.name}(id: {guild.id})'
 		)
 
+@bot.command(name='imgcn', help='Sends Message with Pool Image Count.')
+async def imgcn(ctx, pool=None):
+	validPools = os.listdir(imgPath)
+	if pool not in validPools and pool is not None:
+		await ctx.send("Error: Invalid pool for image count.\nUse `!img opt` to list pools.")
+		return
+
+	if pool is None: # return total image count
+		imgCount = 0
+		for vPool in validPools: # choose random from all directories
+			imgCount += len(listdir_no_hidden(f"{imgPath}{vPool}"))
+		await ctx.send(f"Total Image Count: {imgCount}")
+	else: # return pool image count
+		imgCount = len(listdir_no_hidden(f"{imgPath}{pool}"))
+		await ctx.send(f"Image Count for {pool} pool: {imgCount}")
+	return
+
 @bot.command(name='imgin', help='Upload Image into Pool.')
 @commands.has_role(uploadRole)
 async def imgin(ctx, pool=None):
 	validPools = os.listdir(imgPath)
-	if pool == None:
+	if pool is None:
 		pool = "misc"
 
 	if pool not in validPools:
 		await ctx.send("Error: Invalid pool for upload.\nUse `!img opt` to list pools.")
 		return
 
-	try:
-		url = ctx.message.attachments[0].url
-	except IndexError:
-		print("Error: No Image.")
-		await ctx.send("Error: Need to upload one (1) image file.")
-		return
-	else:
-		if url[0:26] != "https://cdn.discordapp.com":
-			await ctx.send("Error: Bad URL.")
+	for attachment in ctx.message.attachments:
+		try:
+			url = attachment.url
+		except IndexError:
+			print("Error: No Image.")
+			await ctx.send("Error: Need to upload one (1) image file.")
 			return
-		
-		r = requests.get(url, stream=True)
-		#if r.headers['content-length'] > ImageSizeCap:
-		#	await ctx.send(f"Error. File exceeded cap: {imageSizeCap} B")
-		#	return
-		if r.headers['content-type'] not in imageFormats:
-			await ctx.send(f"Error: File must be of types: {imageFormats}")
-			return
-		print(2)
-		imageName = f"{uuid.uuid4()}.{r.headers['content-type'][6:]}"
-		imageName = f"{imgPath}{pool}/{imageName}"
-		with open(imageName, 'wb') as outFile:
-			print('Saving image: ' + imageName)
-			shutil.copyfileobj(r.raw, outFile)
-			await ctx.send(f"Success: Image uploaded to pool: {pool}")
+		else:
+			if url[0:26] != "https://cdn.discordapp.com":
+				await ctx.send("Error: Bad URL.")
+				return
+	
+			r = requests.get(url, stream=True)
+			#if r.headers['content-length'] > ImageSizeCap:
+			#	await ctx.send(f"Error. File exceeded cap: {imageSizeCap} B")
+			#	return
+			if r.headers['content-type'] not in imageFormats:
+				await ctx.send(f"Error: File must be of types: {imageFormats}")
+				return
+			print(2)
+			imageName = f"{uuid.uuid4()}.{r.headers['content-type'][6:]}"
+			imageName = f"{imgPath}{pool}/{imageName}"
+			with open(imageName, 'wb') as outFile:
+				print('Saving image: ' + imageName)
+				shutil.copyfileobj(r.raw, outFile)
+				await ctx.send(f"Success: Image uploaded to pool: {pool}")
 
 @imgin.error
 async def imgin_error(ctx, error):
@@ -76,7 +94,7 @@ def listdir_no_hidden(path):
 
 @bot.command(name='img', help='Responds with random image.')
 async def img(ctx, pool='all'):
-	validPools = os.listdir(imgPath) + ["misc"]
+	validPools = os.listdir(imgPath)
 	filename = ''
 	
 	if pool == 'opt': # return directory options
