35f7a4bb75fb5aca6b861b4d1940f802504f09ea
vgfang <vincent_fang@sfu.ca>
2021-01-10 16:10:53 -0800

    add admin commands, readme

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # imgpool bot
+Imgpool bot is a discord bot for randomly selecting from image pools and uploading images to image pools on a server. There are 3 levels of permissions in the form of an upload role for uploading images, and an admin role for removing images and modifying image pools. 
+
+## Commands
+- any role
+   + `!img`: returns a random image from any pool
+   + `!img poolname`: returns a random image from `poolname` pool
+   + `!imgop`: returns existing poolnames
+   + `!imgcn`: returns the total image count
+   + `!imgcn poolname`: returns the image count of `poolname` pool
+- upload role
+   + `!imgin`: uploads attached image(s) to `misc` pool 
+   + `!imgin poolname`: uploads attached image(s) to `poolname` pool
+      * supports multiple image uploads (up to 10) on mobile 
+- admin role
+   + `!imgrm filename`:
+      * get filename by pressing `Open original` on an imgbot uploaded image
+      * then select the only the image filename
+      * eg. filename: `aabbdd1d-5873-40b7-b7c1-6bdb5ba2356e.png`
+   + `!imgpadd poolname`: adds a new `poolname` pool
+   + `!imgpmod oldPoolName newPoolName`: renames `oldPoolName` pool to `newPoolName`
+   + `!imgpdel poolname`: delete `poolname` pool and all of its contents
+      * cannot delete `misc`
+
+## Deployment on GNU/Linux
+1. `git clone` the repository
+2. Install `tmux` on your distribution for running the bot 24/7. eg. `sudo apt install tmux`
+3. Create a Discord bot at the Discord Developer Portal and add the bot to your desired server
+4. Use your Discord Bot Token to fill in the `.env` variable
+5. Fill out the roles for `UPLOAD_ROLE` and `ADMIN_ROLE` in `.env`
+6. Activate the virtualenv: `source env/bin/activate`
+7. Run the bot using `tmux` so that it does not terminate on logout: 
+   - `tmux`
+   - `source env/bin/activate`
+   - `python3 bot.py`
+   - `Ctrl+b`, `d`
+8. To shut down the bot:
+   - `tmux`
+   - `Ctrl+b`, `s`
+   - select tmux session with the running bot
+   - `x`, `y`
--- bot.py
+++ bot.py
@@ -18,8 +18,61 @@ intents.members = True
 bot = commands.Bot(command_prefix='!')
 
 imgPath = './img_pools/'
-imageFormats = ("image/png", "image/jpeg", "image/jpg", "image/gif")
+imageFormats = ("image/png", "image/jpeg", "image/jpg", "image/gif", "image/webp")
 uploadRole = os.getenv('UPLOAD_ROLE')
+adminRole = os.getenv('ADMIN_ROLE')
+
+# returns filenames within a given directory, excluding hidden unix files
+def listdir_no_hidden(path):
+    return list(filter(lambda x: not x.startswith('.'), os.listdir(path)))
+
+# checks if filename is a safe input
+def check_filename(filename):
+	illegalChars = ['..','\\',' ','/']
+	if filename is None:
+		return f"Error: Filename is empty."
+		
+	for x in illegalChars:
+		if x in filename:
+			return f"Error: Unsafe filename for deletion."
+	return "Success"
+
+# checks if poolname is valid and if the pool already exists
+def check_poolname_and_exists(validPools, pool):
+	check = check_poolname(pool)
+	if check != "Success":
+		return check
+	if pool not in validPools:
+		return f"Error: poolname {pool} does not exist.\nUse `!imgop` to list pools."
+	return "Success"
+
+# checks if poolname is valid and if the pool does not exist
+def check_poolname_and_not_exists(validPools, pool):
+	check = check_poolname(pool)
+	if check != "Success":
+		return check
+	if pool in validPools:
+		return f"Error: poolname {pool} already exists.\nUse `!imgop` to list pools."
+	return "Success"
+
+# checks if poolname is valid
+def check_poolname(pool):
+	illegalChars = ['/','\'','\"',' ','\t','\n','.','\\','..']
+	illegalPools = ['misc']
+	minPoolLen = 3
+	maxPoolLen = 12
+
+	if pool in illegalPools:
+		return f"Error: poolname `{pool}` is illegal."
+	for x in illegalChars:
+		if x in pool:
+			return f"Error: poolname `{pool}` contains an invalid character: `{illegalChars}`."
+	if len(pool) > maxPoolLen:
+		return f"Error: poolname `{pool}` is longer than `{maxPoolLen}` characters."
+	if len(pool) < minPoolLen:
+		return f"Error: poolname `{pool}` is shorter than `{minPoolLen}` characters."
+
+	return "Success"
 
 @bot.event
 async def on_ready():
@@ -29,38 +82,79 @@ async def on_ready():
 			f'{guild.name}(id: {guild.id})'
 		)
 
+# PUBLIC COMMANDS
+# Commands available to any role.
+
+@bot.command(name='imgop', help='Lists possible pool options.')
+async def imgop(ctx):
+	validPools = listdir_no_hidden(imgPath)
+	await ctx.send(f"Valid image pools: `{validPools}`")
+	return
+
 @bot.command(name='imgcn', help='Sends Message with Pool Image Count.')
 async def imgcn(ctx, pool=None):
-	validPools = os.listdir(imgPath)
+	validPools = listdir_no_hidden(imgPath)
 	if pool not in validPools and pool is not None:
-		await ctx.send("Error: Invalid pool for image count.\nUse `!img opt` to list pools.")
+		await ctx.send("Error: Invalid pool for image count.\nUse `!imgop` to list pools.")
 		return
 
 	if pool is None: # return total image count
 		imgCount = 0
 		for vPool in validPools: # choose random from all directories
 			imgCount += len(listdir_no_hidden(f"{imgPath}{vPool}"))
-		await ctx.send(f"Total image count: {imgCount}")
+		await ctx.send(f"Total image count: `{imgCount}`")
 	else: # return pool image count
 		imgCount = len(listdir_no_hidden(f"{imgPath}{pool}"))
-		await ctx.send(f"Image count for {pool} pool: {imgCount}")
+		await ctx.send(f"Image count for `{pool}` pool: `{imgCount}`")
 	return
 
+@bot.command(name='img', help='Responds with random image.')
+async def img(ctx, pool=None):
+	validPools = listdir_no_hidden(imgPath)
+	filename = ''
+		
+	if pool != None and not pool in validPools:
+		await ctx.send("Improper pool input.\nUse `!imgop` to see valid image pools.")
+		return
+
+	if pool == None:
+		pool = ''
+		allImgs = []
+		for vPool in validPools: # choose random from all directories
+			vPoolImgs = listdir_no_hidden(f"{imgPath}{vPool}")
+			allImgs += list(map(lambda x: os.path.join(str(vPool),x),vPoolImgs))
+		if len(allImgs) == 0:
+			await ctx.send("Error: no images.")
+			return
+		filename = f"{imgPath}{random.choice(allImgs)}"
+	else:
+		imgs = listdir_no_hidden(imgPath+pool)
+		if len(imgs) == 0:
+			await ctx.send(f"Error: empty pool `{pool}`.")
+			return
+		filename = f"{imgPath}{pool}/{random.choice()}"
+
+	# add to ctx.send for filename: filename[len(imgPath):],
+	await ctx.send(file=discord.File(filename))
+	return filename
+
+# UPLOAD COMMANDS
+# Commands available to upload and admin roles.
+
 @bot.command(name='imgin', help='Upload Image into Pool.')
-@commands.has_role(uploadRole)
+@commands.has_any_role(uploadRole, adminRole)
 async def imgin(ctx, pool=None):
-	validPools = os.listdir(imgPath)
+	validPools = listdir_no_hidden(imgPath)
 	if pool is None:
 		pool = "misc"
 
 	if pool not in validPools:
-		await ctx.send("Error: Invalid pool for upload.\nUse `!img opt` to list pools.")
+		await ctx.send("Error: Invalid pool for upload.\nUse `!imgop` to list pools.")
 		return
 
 	if len(ctx.message.attachments) == 0:
 		print("Error: No Image.")
 		await ctx.send("Error: Need to upload one (1) image file.")
-		return
 
 	for attachment in ctx.message.attachments:
 		url = attachment.url
@@ -70,46 +164,93 @@ async def imgin(ctx, pool=None):
 
 		r = requests.get(url, stream=True)
 		if r.headers['content-type'] not in imageFormats:
-			await ctx.send(f"Error: File must be of types: {imageFormats}")
+			await ctx.send(f"Error: File must be of types: `{imageFormats}`")
 			return
 		imageName = f"{uuid.uuid4()}.{r.headers['content-type'][6:]}"
 		imageName = f"{imgPath}{pool}/{imageName}"
 		with open(imageName, 'wb') as outFile:
-			print('Saving image: ' + imageName)
 			shutil.copyfileobj(r.raw, outFile)
-			await ctx.send(f"Success: Image uploaded to pool: {pool}")
+			await ctx.send(f"Success: Image uploaded to pool: `{pool}`")
 
+
+# ADMIN COMMANDS
+# Commands available to admin role.
+@bot.command(name='imgrm', help='Removes image using filename')
+@commands.has_any_role(adminRole)
+async def imgrm(ctx, filename=None):
+	check = check_filename(filename)
+	if check != "Success":
+		return check
+
+	validPools = listdir_no_hidden(imgPath)
+	for vPool in validPools: # choose random from all directories
+		vPoolImgs = listdir_no_hidden(f"{imgPath}{vPool}")
+		if filename in vPoolImgs:
+			os.remove(f"{imgPath}{vPool}/{filename}")
+			await ctx.send(f"Success: `{vPool}/{filename}` removed.")
+			return
+	await ctx.send(f"Error: File not found.")
+
+
+@bot.command(name='imgpadd', help='Adds a new pool if available.')
+@commands.has_any_role(adminRole)
+async def imgpadd(ctx, pool=None):
+	validPools = listdir_no_hidden(imgPath)
+	check = check_poolname_and_not_exists(validPools, pool)
+	if check != "Success":
+		await ctx.send(check)
+		return
+	try:
+		os.mkdir(f"{imgPath}{pool}")
+	except:
+	    ctx.send(f"Internal Error: Could not make pool `{pool}`.")
+	    raise
+
+	await ctx.send(f"Success: new pool `{pool}` created.")
+
+@bot.command(name='imgpmod', help='Renames a pool.')
+async def imgpmod(ctx, oldPool=None, newPool=None):
+	validPools = listdir_no_hidden(imgPath)
+	oldCheck = check_poolname_and_exists(validPools, oldPool)
+	if oldCheck != "Success":
+		await ctx.send(oldCheck)
+
+	newCheck = check_poolname_and_not_exists(validPools, newPool)
+	if newCheck != "Success":
+		await ctx.send(newCheck)
+		return
+
+	os.rename(f"{imgPath}{oldPool}", f"{imgPath}{newPool}")
+	await ctx.send(f"Success: pool `{oldPool}` renamed to `{newPool}`.")
+
+@bot.command(name='imgpdel', help='Deletes a pool and its contents.')
+@commands.has_any_role(adminRole)
+async def imgpdel(ctx, pool=None):
+	validPools = listdir_no_hidden(imgPath)
+	
+	check = check_poolname_and_exists(validPools, pool)
+	if check != "Success":
+		await ctx.send(check)
+		return
+
+	try:
+		shutil.rmtree(f"{imgPath}{pool}")
+	except:
+		await ctx.send("Internal Error: pool `{pool}` deletion failure.")
+		return
+	await ctx.send(f"Success: pool `{pool}` has been deleted.")
+
+# ERROR CHECKING
 @imgin.error
 async def imgin_error(ctx, error):
 	if isinstance(error, commands.MissingRole):
-		await ctx.send(f"Need role: {uploadRole} to upload images.")
+		await ctx.send(f"Need role: `{uploadRole}` to upload images.")
 
-def listdir_no_hidden(path):
-    return list(filter(lambda x: not x.startswith('.'), os.listdir(path)))
-
-@bot.command(name='img', help='Responds with random image.')
-async def img(ctx, pool='all'):
-	validPools = os.listdir(imgPath)
-	filename = ''
-	
-	if pool == 'opt': # return directory options
-		await ctx.send("Valid image pools: " + str(validPools))
-		return
-	if pool != 'all' and not pool in validPools:
-		await ctx.send("Improper pool input.\nUse `!img opt` to see valid image pools.")
-		return
-
-	if pool == 'all':
-		pool = ''
-		allImgs = []
-		for vPool in validPools: # choose random from all directories
-			vPoolImgs = listdir_no_hidden(f"{imgPath}{vPool}")
-			allImgs += list(map(lambda x: os.path.join(str(vPool),x),vPoolImgs))
-		filename = f"{imgPath}{random.choice(allImgs)}"
-	else:
-		filename = f"{imgPath}{pool}/{random.choice(listdir_no_hidden(imgPath+pool))}"
-
-	await ctx.send(file=discord.File(filename))
-	return filename
+@imgpadd.error
+@imgpmod.error
+@imgpdel.error
+async def on_pool_fn_error(ctx, error):
+	if isinstance(error, (commands.MissingAnyRole)):
+		await ctx.send(f"Insufficient Permissions.\nNeed role: `{adminRole}` for image deletion and image pool management.")
 
 bot.run(TOKEN)
