080f0ebba4b5117bd2ca3686cb291d07c464705d
vgfang <vincent_fang@sfu.ca>
2021-05-11 17:54:20 -0700

    added imgpmov, imgpmod, imgtrun, imgtlist, imgtstop, imgtstopall functions. new automatic image-posting task feature, updated readme

--- README.md
+++ README.md
@@ -1,27 +1,33 @@
 # imgpool bot
-Imgpool bot is a discord bot for uploading images to image pools on a server and randomly selecting from them with commands. There are 3 levels of permissions in the form of an upload role for uploading images, and an admin role for removing images and modifying image pools. 
+Imgpool bot is a discord bot for uploading images to image pools on a server and randomly selecting from them with commands. There are 3 levels of permissions in the form of an upload role for uploading images, and an admin role for removing images and modifying image pools. Automatic image posting tasks can started and stopped be run synchonously.
 
 ## Commands
 - any role
    + `!img`: returns a random image from any pool
-   + `!img poolname`: returns a random image from `poolname` pool
-   + `!imgop`: returns existing poolnames
+      * `!img poolName`: returns a random image from `poolName` pool
+   + `!imgop`: returns existing poolNames
    + `!imgcn`: returns the total image count
-   + `!imgcn poolname`: returns the image count of `poolname` pool
+      * `!imgcn poolName`: returns the image count of `poolName` pool
    + `!imghp`: returns list of commands
 - upload role
    + `!imgin`: uploads attached image(s) to `misc` pool 
-   + `!imgin poolname`: uploads attached image(s) to `poolname` pool
+   + `!imgin poolName`: uploads attached image(s) to `poolName` pool
       * supports multiple image uploads (up to 10) on mobile 
 - admin role
    + `!imgrm filename`:
       * get filename by pressing `Open original` on an imgbot uploaded image
       * then select the only the image filename
       * eg. filename: `aabbdd1d-5873-40b7-b7c1-6bdb5ba2356e.png`
-   + `!imgpadd poolname`: adds a new `poolname` pool
+   + `!imgpadd poolName`: adds a new `poolName` pool
    + `!imgpmod oldPoolName newPoolName`: renames `oldPoolName` pool to `newPoolName`
-   + `!imgpdel poolname`: delete `poolname` pool and all of its contents
-      * cannot delete `misc`
+   + `!imgpmov oldPoolName newPoolName`: moves `oldPoolName` images to `newPoolname`
+   + `!imgpdel poolName`: delete `poolName` pool and all of its contents
+   + `!imgtrun`: starts automatic image-posting task at a fixed time interval
+      * `!imgtrun numOfMinutes`: with user-specified time interval
+      * `!imgtrun numOfMinutes poolName`: user-specified time interval and pool
+   + `imgtlist`: list all automatic image-posting tasks
+   + `imgtstop taskId`: stop automatic image-posting task using its taskId
+   + `imgtstopall`: stop all automatic image-posting tasks
 
 ## Deployment on GNU/Linux
 1. `git clone` the repository
--- bot.py
+++ bot.py
@@ -2,9 +2,11 @@
 import os
 import random
 import requests
+import asyncio
 import uuid
 import shutil
 import discord
+import string
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -21,6 +23,8 @@ imgPath = './img_pools/'
 imageFormats = ("image/png", "image/jpeg", "image/jpg", "image/gif", "image/webp")
 uploadRole = os.getenv('UPLOAD_ROLE')
 adminRole = os.getenv('ADMIN_ROLE')
+bot.imgRunTasks = []
+bot.maxImgRunTasks = 6
 
 # returns filenames within a given directory, excluding hidden unix files
 def listdir_no_hidden(path):
@@ -113,11 +117,11 @@ async def img(ctx, pool=None):
 	validPools = listdir_no_hidden(imgPath)
 	filename = ''
 		
-	if pool != None and pool not in validPools:
+	if pool is not None and pool not in validPools:
 		await ctx.send("Error: Improper pool input.\nUse `!imgop` to see valid image pools.")
 		return
 
-	if pool == None:
+	if pool is None:
 		pool = ''
 		allImgs = []
 		for vPool in validPools: # choose random from all directories
@@ -169,7 +173,7 @@ async def imgin(ctx, pool=None):
 		filedirname = f"{imgPath}{pool}/{filename}"
 		with open(filedirname, 'wb') as outFile:
 			shutil.copyfileobj(r.raw, outFile)
-			await ctx.send(f"Success: `{pool}`/`{filename}` uploaded.")
+			await ctx.send(f"Success: uploaded `{pool}`/`{filename}`")
 
 
 # ADMIN COMMANDS
@@ -187,7 +191,7 @@ async def imgrm(ctx, filename=None):
 		vPoolImgs = listdir_no_hidden(f"{imgPath}{vPool}")
 		if filename in vPoolImgs:
 			os.remove(f"{imgPath}{vPool}/{filename}")
-			await ctx.send(f"Success: `{vPool}/{filename}` removed.")
+			await ctx.send(f"Success: removed `{vPool}`/`{filename}`")
 			return
 	await ctx.send(f"Error: File not found.")
 
@@ -209,30 +213,65 @@ async def imgpadd(ctx, pool=None):
 	await ctx.send(f"Success: new pool `{pool}` created.")
 
 @bot.command(name='imgpmod', help='Renames a pool.')
+@commands.has_any_role(adminRole)
 async def imgpmod(ctx, oldPool=None, newPool=None):
+	if oldPool == 'misc' or newPool == 'misc':
+		await ctx.send("Error: `misc` pool selected.")
+		return
+
 	validPools = listdir_no_hidden(imgPath)
+	
 	oldCheck = check_poolname_and_exists(validPools, oldPool)
 	if oldCheck != "Success":
 		await ctx.send(oldCheck)
+		return
 
 	newCheck = check_poolname_and_not_exists(validPools, newPool)
 	if newCheck != "Success":
 		await ctx.send(newCheck)
 		return
 
+
 	os.rename(f"{imgPath}{oldPool}", f"{imgPath}{newPool}")
 	await ctx.send(f"Success: pool `{oldPool}` renamed to `{newPool}`.")
+
+@bot.command(name='imgpmov', help='Moves all images from a pool')
+@commands.has_any_role(adminRole)
+async def imgpmov(ctx, oldPool=None, newPool=None):
+	if oldPool == 'misc':
+		await ctx.send("Error: `misc` pool selected as oldPool")
+		return
+	
+	validPools = listdir_no_hidden(imgPath) 
+	oldCheck = check_poolname_and_exists(validPools, oldPool)
+	if oldCheck != "Success":
+		await ctx.send(oldCheck)
+		return
+
+	imgs = listdir_no_hidden(imgPath+oldPool)
+	if len(imgs) == 0:
+		await ctx.send(f"Error: Empty oldPool `{oldPool}`.")
+		return
+
+	newCheck = check_poolname_and_exists(validPools, newPool)
+	# exception that newPool can be misc
+	if newCheck != "Success" and not newPool == 'misc':
+		await ctx.send(newCheck)
+		return
+
+	oldFiles = os.listdir(f"{imgPath}{oldPool}")
+	for fileName in oldFiles:
+		shutil.move(os.path.join(f"{imgPath}{oldPool}", fileName), f"{imgPath}{newPool}")
+	await ctx.send(f"Success: pool `{oldPool}` contents moved  to `{newPool}`.")
 
 @bot.command(name='imgpdel', help='Deletes a pool and its contents.')
 @commands.has_any_role(adminRole)
 async def imgpdel(ctx, pool=None):
 	validPools = listdir_no_hidden(imgPath)
-	
 	check = check_poolname_and_exists(validPools, pool)
 	if check != "Success":
 		await ctx.send(check)
 		return
-
 	try:
 		shutil.rmtree(f"{imgPath}{pool}")
 	except:
@@ -240,15 +279,95 @@ async def imgpdel(ctx, pool=None):
 		return
 	await ctx.send(f"Success: Pool `{pool}` has been deleted.")
 
+# internal coroutine for imgtrun
+async def imgrun(ctx, taskId, minutes, pool=None): # coroutine to send images to a channel
+	counter = 1
+	if pool is None:
+		await ctx.send(f"Success: initialized imgtrun task `{taskId}` with `{minutes}` minute timer.")
+	else:
+		await ctx.send(f"Success: initialized imgtrun task `{taskId}` with pool `{pool}` and `{minutes}` minute timer.")
+	try:
+		while True:
+			await ctx.send(f"Image: imgtrun taskId=`{taskId}` counter: `{counter}`")
+			await img(ctx, pool)
+			await asyncio.sleep(minutes * 60)
+			counter += 1
+	except asyncio.CancelledError:
+		await ctx.send(f"Success: canceled imgtrun task `{taskId}`")
+
+@bot.command(name='imgtrun', help='Start imgtrun tasks for automatical image posting based on a time interval.')
+@commands.has_any_role(adminRole)
+async def imgtrun(ctx, minutes=1, pool=None):
+	if not isinstance(minutes,int) or minutes < 1:
+		await ctx.send(f"Error: minutes must be an integer greater than `0`.")
+		return
+	
+	validPools = listdir_no_hidden(imgPath)
+	if pool is not None and pool not in validPools:
+		await ctx.send("Error: Improper pool input.\nUse `!imgop` to see valid image pools.")
+		return
+
+	# id for stopping
+	taskId = random.choice(string.ascii_lowercase) + random.choice(string.ascii_lowercase) + random.choice(string.ascii_lowercase) + str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9))
+
+	if len(bot.imgRunTasks) > bot.maxImgRunTasks:
+		await ctx.send("Error: Maximum number of imgtrun tasks (`6`) have been exceeded. Stop tasks using `!imgpstp` or `!imgpstpall`")
+	# add to global list of current imgtrun, tuple with (id, minutes, pool, coroutine)
+	bot.imgRunTasks.append((taskId, minutes, pool,(asyncio.create_task(imgrun(ctx, taskId, minutes, pool)))))
+
+@bot.command(name='imgtstop', help='Stop a imgtrun task')
+@commands.has_any_role(adminRole)
+async def imgtstop(ctx, taskId=None):
+	if taskId is None:
+		await ctx.send(f"Error: Inter a valid imgtrun taskId. List options with `!imgtlist`")
+		return
+	if bot.imgRunTasks == []:
+		await ctx.send(f"Error: There are no imgtruns to cancel.")
+		return
+	for task in bot.imgRunTasks:
+		print(task[0])
+		if task[0] == taskId:
+			task[3].cancel()
+			return
+	await ctx.send(f"Error: Inter a valid imgtrun taskId. List options with `!imgtlist`")
+	return
+
+@bot.command(name='imgtstopall', help='Stop all imgtrun tasks.')
+@commands.has_any_role(adminRole)
+async def imgtstopall(ctx, taskId=None):
+	if bot.imgRunTasks == []:
+		await ctx.send(f"Error: There are no imgtruns to cancel.")
+		return
+	for task in bot.imgRunTasks:
+		task[3].cancel()
+	await ctx.send(f"Success: All imgtrun tasks are cancelled")
+	return
+
+@bot.command(name='imgtlist', help='List imgtrun Tasks.')
+@commands.has_any_role(adminRole)
+async def imgtlist(ctx):
+	print(bot.imgRunTasks)
+	if bot.imgRunTasks == []:
+		await ctx.send(f"There are no imgtruns currently running.")
+	message = f"Current imgtrun tasks:\n"
+	for task in bot.imgRunTasks:
+		message += f"\ttaskId: `{task[0]}` pool: `{task[1]}` minutes: `{task[2]}`\n"
+	await ctx.send(message)
+
 # ERROR CHECKING
 @imgin.error
 async def imgin_error(ctx, error):
 	if isinstance(error, commands.MissingAnyRole):
 		await ctx.send(f"Error: Need role `{uploadRole}` to upload images.")
 
+@imgrm.error
 @imgpadd.error
 @imgpmod.error
 @imgpdel.error
+@imgtrun.error
+@imgtstop.error
+@imgtstopall.error
+@imgtlist.error
 async def on_pool_fn_error(ctx, error):
 	if isinstance(error, (commands.MissingAnyRole)):
 		await ctx.send(f"Error: Need role `{adminRole}` for image deletion and image pool management.")
